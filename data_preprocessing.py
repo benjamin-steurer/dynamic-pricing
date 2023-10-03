72b3cebfe82c0ee2129735da506c98ee38ef79b4
Benjamin <info@steurerconsulting.de>
2023-10-03 10:55:34 +0200

    first prototype with RL and DQN

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -14,10 +14,12 @@ def load_and_preprocess_data():
     
     scaler = MinMaxScaler()
     features_to_scale = ['ReturnAfterPPC', 'Price']
-    df_filtered[features_to_scale] = scaler.fit_transform(df_filtered[features_to_scale])
+    df_filtered.loc[:, features_to_scale] = scaler.fit_transform(df_filtered[features_to_scale])
     
-    X = df_filtered[['ReturnAfterPPC']]
+    X = df_filtered[['ReturnAfterPPC', 'Price']]
     y = df_filtered['Price']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
     
     return df_filtered, X_train, X_test, y_train, y_test
+
--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from dynamic_pricing_env import DynamicPricingEnv
 from dqn_model import DQN
 from replay_buffer import ReplayBuffer
 import random
+import numpy as np
+
+# Check for GPU availability
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 env = DynamicPricingEnv()
 
 state = env.reset()
-print("Initial state:", state)
 
-# Initialize replay buffer and epsilon value
 replay_buffer = ReplayBuffer(1000)
 epsilon = 0.1
 
@@ -23,69 +26,58 @@ print("Reward:", reward)
 
 df_filtered, X_train, X_test, y_train, y_test = load_and_preprocess_data()
 
-input_dim = 2  # This should match the shape of the state
-output_dim = 1  # Price
-model = DQN(input_dim, output_dim)
+input_dim = 2 
+output_dim = 1  # Number of possible actions (prices)
+model = DQN(input_dim, output_dim).to(device)  # Move model to device
 
-# Initialize optimizer and loss function
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
-# Convert data to PyTorch tensors
-X_train_tensor = torch.FloatTensor(X_train.values)
-y_train_tensor = torch.FloatTensor(y_train.values).view(-1, 1)
+# Move data to device
+X_train_tensor = torch.FloatTensor(X_train.values).to(device)
+y_train_tensor = torch.FloatTensor(y_train.values).view(-1, 1).to(device)
 
-# Training loop
 for epoch in range(1000):
     optimizer.zero_grad()
     output = model(X_train_tensor)
     loss = criterion(output, y_train_tensor)
     loss.backward()
     optimizer.step()
-    if epoch % 100 == 0:
+    if epoch % 1000 == 0:
         print(f"Epoch {epoch}, Loss: {loss.item()}")
 
-# Training loop with environment and experience replay
 batch_size = 32
 for episode in range(1000):
     state = env.reset()
     episode_reward = 0
 
     for t in range(200):
-        # Epsilon-greedy action selection
         if random.random() < epsilon:
             action = env.action_space.sample()
         else:
-            state_tensor = torch.FloatTensor(state).unsqueeze(0)
+            state_tensor = torch.FloatTensor(np.array([state])).unsqueeze(0).to(device)
             q_values = model(state_tensor)
             action = q_values.argmax().item()
 
-        # Take action and get reward
         next_state, reward, done, _ = env.step(action)
-
-        # Store experience in replay buffer
         replay_buffer.push(state, action, reward, next_state, done)
-
         state = next_state
         episode_reward += reward
 
-        # Train the model if enough experiences are available
         if len(replay_buffer) >= batch_size:
             state_batch, action_batch, reward_batch, next_state_batch, done_batch = replay_buffer.sample(batch_size)
 
-            # Convert to PyTorch tensors
-            state_batch = torch.FloatTensor(state_batch)
-            action_batch = torch.LongTensor(action_batch)
-            reward_batch = torch.FloatTensor(reward_batch)
-            next_state_batch = torch.FloatTensor(next_state_batch)
-            done_batch = torch.FloatTensor(done_batch)
+            # Move batch data to device
+            state_batch = torch.FloatTensor(state_batch).to(device)
+            action_batch = torch.LongTensor(action_batch).to(device)
+            reward_batch = torch.FloatTensor(reward_batch).to(device)
+            next_state_batch = torch.FloatTensor(next_state_batch).to(device)
+            done_batch = torch.FloatTensor(done_batch).to(device)
 
-            # Compute Q-values and next Q-values
-            q_values = model(state_batch).gather(1, action_batch.unsqueeze(1)).squeeze()
+            q_values = model(state_batch).squeeze(1)
             next_q_values = model(next_state_batch).max(1)[0]
             expected_q_values = reward_batch + 0.99 * next_q_values * (1 - done_batch)
 
-            # Compute loss and update model
             loss = criterion(q_values, expected_q_values.detach())
             optimizer.zero_grad()
             loss.backward()
@@ -94,3 +86,48 @@ for episode in range(1000):
     if episode % 10 == 0:
         print(f"Episode {episode}, Total Reward: {episode_reward}")
 
+
+
+
+while True:  # Infinite loop to keep the program running
+    latest_date = df_filtered['Date'].max()
+    latest_price = df_filtered[df_filtered['Date'] == latest_date]['Price'].values[0]
+    latest_price_float = float(latest_price)
+
+    current_state_tensor = torch.FloatTensor([[latest_price_float, 0.0]]).to(device)
+
+    with torch.no_grad():
+        suggested_price_tensor = model(current_state_tensor)
+    suggested_price = suggested_price_tensor.item()
+    print(f"Suggested price: {suggested_price}")
+
+    # New code to accept real-world contribution margin
+    real_world_margin = float(input("Enter the real-world contribution margin for the suggested price: "))
+
+    # Use the real-world margin as the reward for the last action
+    replay_buffer.push(state, suggested_price, real_world_margin, next_state, done)
+
+    # Update your model based on the new data in the replay buffer
+    if len(replay_buffer) >= batch_size:
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = replay_buffer.sample(batch_size)
+
+        # Move batch data to device
+        state_batch = torch.FloatTensor(state_batch).to(device)
+        action_batch = torch.LongTensor(action_batch).to(device)
+        reward_batch = torch.FloatTensor(reward_batch).to(device)
+        next_state_batch = torch.FloatTensor(next_state_batch).to(device)
+        done_batch = torch.FloatTensor(done_batch).to(device)
+
+        q_values = model(state_batch).squeeze(1)
+        next_q_values = model(next_state_batch).max(1)[0]
+        expected_q_values = reward_batch + 0.99 * next_q_values * (1 - done_batch)
+
+        loss = criterion(q_values, expected_q_values.detach())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Add a way to break the loop if needed
+    user_input = input("Do you want to continue? (y/n): ")
+    if user_input.lower() == 'n':
+        break
